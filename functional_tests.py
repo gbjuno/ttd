76a8e0471d0380e691908e43cac79ffee9ca60c5
junlun <junlun.fan@gmail.com>
2014-07-10 22:27:22 +0800

    到处重复的代码

--- functional_tests.py
+++ functional_tests.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):  #
 
@@ -32,14 +33,13 @@ class NewVisitorTest(unittest.TestCase):  #
         inputbox.send_keys('Buy peacock feathers')
 
         inputbox.send_keys(Keys.ENTER)
-
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-                any(row.text == '1: Buy peacock feathers' for row in rows),
-                "New to-do item did not appear in table"
-                )
-        
+        #self.assertTrue(
+        #any(row.text == '1: Buy peacock feathers' for row in rows),
+        #"New to-do item did not appear in table,
+        #the text is %s" % (table.text,))
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
         self.fail('Finish the test!')  #
 
 if __name__ == '__main__':  #
