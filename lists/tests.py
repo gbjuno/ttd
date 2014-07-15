361901a0cbae050531fdfa460a33b80d979c2515
junlun <junlun.fan@gmail.com>
2014-07-15 15:32:37 +0800

    还需要继续改

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,7 @@ from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from lists.views import home_page
 from django.template.loader import render_to_string
-from lists.models import Item
+from lists.models import Item, List
 # Create your tests here.
 
 class HomePageTest(TestCase):
@@ -24,26 +24,6 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(),expected_html)
 
-    def test_saving_and_retrieving_items(self):
-        """
-        测试models中的Item数据项
-        """
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.save()
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(second_saved_item.text, 'Item the second')
-
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
         """
@@ -56,8 +36,9 @@ class ListViewTest(TestCase):
         """
         测试/lists/the-only-list-in-the-world的处理函数的返回是否包含数据项Item的值
         """
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
@@ -88,3 +69,33 @@ class NewListTest(TestCase):
             )
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+
+class ListAndItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.list = list_
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.list = list_
+        second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
+
+    
