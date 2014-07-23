69c8d725b8ab93d3f3e35cae8da27554218d5fdf
junlun <junlun.fan@gmail.com>
2014-07-23 14:58:29 +0800

    添加实际部署的测试用例

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,9 +4,23 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 import time
-
+import sys
 
 class NewVisitorTest(StaticLiveServerCase):  #
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super.tearDownClass()
 
     def setUp(self):  #
         self.browser = webdriver.Firefox()
@@ -24,7 +38,7 @@ class NewVisitorTest(StaticLiveServerCase):  #
     def test_can_start_a_list_and_retrieve_it_later(self):  #
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)  #
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -55,7 +69,7 @@ class NewVisitorTest(StaticLiveServerCase):  #
         self.browser.quit()
         self.browser = webdriver.Firefox()
 
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers',  page_text)
         self.assertNotIn('make a fly', page_text)
@@ -72,7 +86,7 @@ class NewVisitorTest(StaticLiveServerCase):  #
 
     def test_layout_and_styling(self):
 
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024,768)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
